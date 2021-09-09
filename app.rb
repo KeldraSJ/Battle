55fb1ff6c3206787ab11059a18ad832bc2d90f2b
Keldra <KeldraSJ@gmail.com>
2021-09-09 15:07:49 +0100

    refactoring of entering players using PRG pattern

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 class Battle < Sinatra::Base
-  
+  enable :sessions 
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -12,10 +13,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end 
 
   run! if app_file == $0
 end
